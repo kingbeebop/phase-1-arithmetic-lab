c0dbb3a965cdc65e830df6c2ad91c869ff56d126
Brendan Ward <kingbeebop@gmail.com>
2022-05-16 15:55:25 -0400

    commit tree 482ea81253117b74943006b18a335f2ab22e438d

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+let num1 = 62
+let num2 = 1
+let num3 = 10
+let num4 = 6
+let numbers = [1,2,3,4,5,20]
+
+let multiply = num1 * num2
+
+let random = Number.parseInt(Math.random() * 10 + 1) 
+
+let mod = num3 % num4
+
+let max = Math.max(1,2,20)
